656a86e29b2f7608b0c50ca3ea15b6e57c947987
Rian Block Serena <78942090+rianblockserena@users.noreply.github.com>
2021-07-20 11:03:39 -0300

    adicao dos bots na lista

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #encoding: utf-8
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
+from Bots.BotFeliz import BotFeliz
+from Bots.BotTriste import BotTriste
 
 ###construa a lista de bots disponíveis aqui
 lista_bots = [BotZangado("Vegeta"), BotFeliz("Qiyana"), BotTriste("Teobaldo")]
