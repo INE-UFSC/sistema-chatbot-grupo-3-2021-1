e18c6557edf59ab11384fa15a4e465bdeb95d84e
Mauricio Konrath <83033652+mauriciokonrath@users.noreply.github.com>
2021-07-20 11:26:35 -0300

    Update BotFeliz.py

--- Bots/BotFeliz.py
+++ Bots/BotFeliz.py
@@ -16,20 +16,20 @@ class BotFeliz(Bot):
         self.__nome = novo_nome
 
     def apresentacao(self):
-        return "Oiiii. Meu nome é {}. :)".format(self.__nome)
+        return f'Oiiii. Meu nome é {self.__nome}. :)'
 
     def mostra_comandos(self):
-        return '1 - Bom dia', '2 - Qual seu nome?', '3 - Quero um conselho', '4 - Adeus'
+        return "1 - Bom dia \n2 - Qual seu nome? \n3 - Quero um conselho \n4 - Adeus"
     
-    def executa_comando(self,cmd):
+    def executa_comando(self, cmd):
         if cmd == '1':
             return 'Bom dia, Bom dia, o sol já nasceu lá na fazendinha.'
         elif cmd == '2':
-            return self.apresentacao()
-        elif cmd == '4':
-            return self.despedida()
+            return 'Oiii, meu nome é Qiyana e estou aqui para lhe ajudar.'
+        elif cmd == '3':
+            return 'SEJA FELIZ. Não ligue para os outros'
         else:
-            return 'Ai amiguinho não tenho essa opção'
+            return self.despedida()
        
     def boas_vindas(self):
         return "Seja bem vindesss :)"
